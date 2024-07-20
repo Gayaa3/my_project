e93b88b6e07d132c4b8274028af7aadd8ebb08e0
Gayaa3 <144539990+Gayaa3@users.noreply.github.com>
2024-07-20 23:10:11 +0530

    Add files via upload

---
+++ Grocery.js
@@ -0,0 +1,13 @@
+
+
+const Grocery = ()=>{
+    return(
+        <div>
+            <div className="text-center font-bold text-2xl p-10">
+                Grocery..🥦
+            </div>
+        </div>
+    )
+}
+
+export default Grocery;
