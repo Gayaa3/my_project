e84e722e7a056b72a70ff8654693c7f12013d0b5
Gayaa3 <144539990+Gayaa3@users.noreply.github.com>
2024-07-20 23:11:46 +0530

    Add files via upload

---
+++ Shimmer.js
@@ -0,0 +1,33 @@
+
+const Shimmer = ()=>{
+    return(
+        <div className="p-8">
+            <div className="flex">
+                <div className="flex">
+                    <div className="rounded-lg p-2 ml-2 mb-2 bg-slate-300 w-44 h-10"></div>
+                    <div className="rounded-lg p-2 mb-2 bg-slate-300 w-20 h-10"></div>
+                </div>
+                <div className="rounded-lg p-2 ml-2 mb-2 bg-slate-300 w-44 h-10"></div>
+            </div>
+            <div className="flex flex-wrap">
+                <div className="w-56 h-80 p-1 m-2 bg-slate-200 rounded-t-3xl"></div>
+                <div className="w-56 h-80 p-1 m-2 bg-slate-200 rounded-t-3xl"></div>
+                <div className="w-56 h-80 p-1 m-2 bg-slate-200 rounded-t-3xl"></div>
+                <div className="w-56 h-80 p-1 m-2 bg-slate-200 rounded-t-3xl"></div>
+                <div className="w-56 h-80 p-1 m-2 bg-slate-200 rounded-t-3xl"></div>
+                <div className="w-56 h-80 p-1 m-2 bg-slate-200 rounded-t-3xl"></div>
+                <div className="w-56 h-80 p-1 m-2 bg-slate-200 rounded-t-3xl"></div>
+                <div className="w-56 h-80 p-1 m-2 bg-slate-200 rounded-t-3xl"></div>
+                <div className="w-56 h-80 p-1 m-2 bg-slate-200 rounded-t-3xl"></div>
+                <div className="w-56 h-80 p-1 m-2 bg-slate-200 rounded-t-3xl"></div>
+                <div className="w-56 h-80 p-1 m-2 bg-slate-200 rounded-t-3xl"></div>
+                <div className="w-56 h-80 p-1 m-2 bg-slate-200 rounded-t-3xl"></div>
+                <div className="w-56 h-80 p-1 m-2 bg-slate-200 rounded-t-3xl"></div>
+                <div className="w-56 h-80 p-1 m-2 bg-slate-200 rounded-t-3xl"></div>
+                <div className="w-56 h-80 p-1 m-2 bg-slate-200 rounded-t-3xl"></div>
+            </div>
+        </div>
+    )
+}
+
+export default Shimmer;
