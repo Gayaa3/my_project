1866c5334489b334a90994bc93773773e46407d9
Gayaa3 <144539990+Gayaa3@users.noreply.github.com>
2024-07-20 23:11:59 +0530

    Add files via upload

---
+++ User.js
@@ -0,0 +1,15 @@
+
+const User = (props)=>{
+
+    return(
+        <div>
+            <div className="text-2xl font-semibold font-mono m-16">
+                <h1>Name : {props.name}</h1>
+                <h1>Location : Nizampatnam</h1>
+                <h1>Contact : gayathrilanke90@gmail.com</h1>
+            </div>
+        </div>
+    )
+}
+
+export default User;
